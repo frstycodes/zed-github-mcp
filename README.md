5e7928373254d31aa2aab9480ca890627204cc49
Sandesh Pandey <sandeshpandeywork@gmail.com>
2025-03-20 18:12:14 +0545

    Initialize Zed GitHub Context Server extension

---
+++ README.md
@@ -0,0 +1,15 @@
+Zed GitHub Context Server
+This extension provides a Model Context Server for GitHub, for use with the Zed AI assistant.
+
+Configuration
+To use the extension, you will need to provide the context server with your GitHub personal access token in your Zed settings.json:
+
+{
+  "context_servers": {
+    "github-context-server": {
+      "settings": {
+        "github_personal_access_token": "your_personal_access_token_here"
+      }
+    }
+  }
+}
---
+++ src/github_mcp.rs
@@ -0,0 +1,56 @@
+use std::env;
+
+use serde::Deserialize;
+use zed_extension_api::{self as zed, Extension, serde_json, settings::ContextServerSettings};
+
+const PACKAGE_NAME: &str = "@modelcontextprotocol/server-github";
+const PACKAGE_VERSION: &str = "2025.3.19";
+const SERVER_PATH: &str = "node_modules/@modelcontextprotocol/server-github/dist/index.js";
+const TOKEN_KEY: &str = "GITHUB_PERSONAL_ACCESS_TOKEN";
+
+#[derive(Debug, Deserialize)]
+struct GithubMCPSettings {
+    github_personal_access_token: String,
+}
+
+struct GithubMCP;
+impl Extension for GithubMCP {
+    fn new() -> Self {
+        Self
+    }
+
+    fn context_server_command(
+        &mut self,
+        _server_id: &zed_extension_api::ContextServerId,
+        project: &zed_extension_api::Project,
+    ) -> zed_extension_api::Result<zed_extension_api::Command> {
+        let version = zed::npm_package_installed_version(PACKAGE_NAME)?;
+        if version.as_deref() != Some(PACKAGE_VERSION) {
+            zed::npm_install_package(PACKAGE_NAME, PACKAGE_VERSION)?;
+        }
+        let settings = ContextServerSettings::for_project("github-context-server", project)?;
+
+        let settings: GithubMCPSettings = match settings.settings {
+            Some(s) => {
+                serde_json::from_value(s).map_err(|e| format!("Failed to parse settings: {}", e))
+            }
+            None => {
+                return Err("Missing 'github_personal_access_token'".to_string());
+            }
+        }?;
+
+        let server_path = env::current_dir()
+            .unwrap()
+            .join(SERVER_PATH)
+            .to_string_lossy()
+            .to_string();
+
+        Ok(zed::Command {
+            command: "node".to_string(),
+            args: vec![server_path],
+            env: vec![(TOKEN_KEY.into(), settings.github_personal_access_token)],
+        })
+    }
+}
+
+zed::register_extension!(GithubMCP);
